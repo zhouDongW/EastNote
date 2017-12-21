c4ce969cacd0b72ecdeedce610406cb89e4e25ee
zhoudongwei <zhoudongwei121@126.com>
2017-12-21 10:15:38 +0800

    项目创建

---
+++ EasyNotePro/EasyNotePro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  EasyNotePro
+//
+//  Created by zhoudongwei on 2017/12/21.
+//  Copyright © 2017年 ZSwift. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
